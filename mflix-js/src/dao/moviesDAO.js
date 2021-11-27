aae46500e314fbb3a165d94ec207f0fea0f67c30
ジェスロ・ジョロラン <40770876+rjeth@users.noreply.github.com>
2021-11-27 20:26:27 +0800

    Chapter 4: Resiliency Ticket: Handling Errors

--- mflix-js/src/dao/moviesDAO.js
+++ mflix-js/src/dao/moviesDAO.js
@@ -402,6 +402,15 @@ export default class MoviesDAO {
       Handle the error that occurs when an invalid ID is passed to this method.
       When this specific error is thrown, the method should return `null`.
       */
+      if (
+        e
+        .toString()
+        .startsWith(
+          "Error: Argument passed in must be a single String of 12 bytes or a string of 24 hex characters",
+        )
+      ) {
+        return null
+      }
 
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
